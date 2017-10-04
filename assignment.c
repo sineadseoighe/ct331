1e45425f6d8a918f2a473d687ce1a327dcc9da1a
sineadseoighe <sineadj1@hotmail.com>
2017-10-04 21:36:19 +0100

    Add files via upload

---
+++ assignment.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(int arg, char* argc[]){
+int a;
+int *b;
+long c;
+double *d;
+char** e;
+printf("Size of int"sizeof(a));
+printf("Size of int**"sizeof(b));
+printf("Size of long"sizeof(c));
+printf("Size of double"sizeof(d));
+printf("Size of char**"sizeof(e));
+
+  printf("Hello assignment1.\n");
+
+  return 0;
+}
